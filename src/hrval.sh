669eef1f8d107a9eb8c5b135b690f9f683b180b4
Sébastien <sebastien.nahelou@gmail.com>
2020-09-02 10:28:52 +0200

    Update hrval.sh

--- src/hrval.sh
+++ src/hrval.sh
@@ -116,13 +116,17 @@ function validate {
 
   echo "Writing Helm release to ${TMPDIR}/${HELM_RELEASE_NAME}.release.yaml"
   if [[ ${HELM_VER} == "v3" ]]; then
-    helmv3 dependency build
+    if [[ "${CHART_PATH}" ]]; then
+      helmv3 dependency build ${CHART_DIR}
+    fi
     helmv3 template ${HELM_RELEASE_NAME} ${CHART_DIR} \
       --namespace ${HELM_RELEASE_NAMESPACE} \
       --skip-crds=true \
       -f ${TMPDIR}/${HELM_RELEASE_NAME}.values.yaml > ${TMPDIR}/${HELM_RELEASE_NAME}.release.yaml
   else
-    helm dependency build
+    if [[ "${CHART_PATH}" ]]; then
+      helmv3 dependency build ${CHART_DIR}
+    fi
     helm template ${CHART_DIR} \
       --name ${HELM_RELEASE_NAME} \
       --namespace ${HELM_RELEASE_NAMESPACE} \
